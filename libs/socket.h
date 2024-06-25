16c394680201bfa8d2e5d8186469359bceb6623b
0marAmez <omar11.amezquita@gmail.com>
2024-06-24 23:09:53 -0500

    socket header file

--- libs/socket.h
+++ libs/socket.h
@@ -1,6 +1,29 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 
-// Declarations go here
+#include <string.h>
+#include <arpa/inet.h>
 
-#endif // EXAMPLE_H
+class Socket{
+    enum protocol{TCP, UDP};
+    private:
+        int     fd; // file descriptor
+        int     port; // Port number
+        struct  sockaddr_in address; // IPv4 address
+        protocol prot_type;
+    public:
+        Socket(int port, protocol prot_type);
+        // Initializes the socket
+        void init_socket();
+        //Binding socket to address and port
+        void init_bind();
+        //Listening on port for connection
+        void init_listen();
+        //handles TCP/UDP connection
+        void handle_connection();
+        //close Socket
+        void close();
+
+};
+
+#endif 
